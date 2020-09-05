179dba736f46c87a25828e000bd950da94b06cdc
zengtianli <zengtianli1@gmail.com>
2020-09-05 14:20:47 +0800

    2 methods daraframe

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-1 made own framedata
+1 made own dataframe 2 method 
+# 1 dataframe, head iloc,info
+2 methods
+## vim macro
+# 2 head
+<++>
+2 head() iloc index location
--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 import pandas as pd
 
 df = pd.DataFrame([
-    [1, 'Hangzhou', 2000],
-    [2, 'Shanghai', 4000],
-    [3, 'Beijing', 5000],
-    [4, 'Guangdong', 3000],
-    [5, 'Shenzhen', 5000],
-    [6, 'Nanjing', 2000],
+    [1, 'Hangzhou',  2000, ],
+    [2, 'Shanghai',  4000, ],
+    [3, 'Beijing',   5000, ],
+    [4, 'Guangdong', 3000, ],
+    [5, 'Shenzhen',  5000, ],
+    [6, 'Nanjing',   2000, ],
 ],
-                  columns=['city id', 'city name', 'lowest salary'])
+    columns=['city id', 'city name', 'average salary'])
 
+df = pd.DataFrame({
+    'city id': [1, 2, 3, 4, 5, 6, ],
+    'city name': ['Hangzhou', 'Shanghai', 'Beijing',  'Guangdong', 'Shenzhen', 'Nanjing', ],
+    'average salary': [2000, 4000, 5000, 3000, 5000, 2000, ],
+})
 print(df)
+# print(df.head(1))
+print(df.iloc[0:2])
+print(df.iloc[0, 2])
+print(df.iloc[0, 1])
